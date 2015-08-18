0b0836fd08d00074dbcc943656df83aac4842761
Jay Byam <jrbyam@alaska.edu>
2015-08-18 09:43:57 -0800

    Changed About page over to a simple WebView

--- src/androidfrontiersci/about/AboutActivity.java
+++ src/androidfrontiersci/about/AboutActivity.java
@@ -7,101 +7,88 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Rect;
 import android.os.Bundle;
-import android.text.Layout;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.LeadingMarginSpan;
-import android.view.LayoutInflater;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewTreeObserver;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidfrontiersci.MainActivity;
 
 import frontsci.android.R;
 
-import java.util.List;
-import java.util.Map;
-
-import androidfrontiersci.ImageProcessor;
-import androidfrontiersci.JsonParser;
-
 /*
-    This is AboutActivity, a class that displays the content of about_page_info defined in
-    JsonParser.java in a ScrollView.
+    This is AboutActivity, a class that displays the About page in a WebView.
     Its layout file is activity_about.xml.
     Its menu file is about.xml.
-    The "Contact Developers" option of the action bar opens an email client to start the message
-    sending process.
 */
 public class AboutActivity extends Activity {
 
-    String email_tag = "[frontierscidevelopers] ";
+    // The class' private variable. its WebView
+    private String link = "http://fsci15.alpinewp.com/about/";
+    private WebView webView;
+    private static AboutProgressDialog progress;
+    private String email_tag = "[frontscidevelopers]";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_about);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-        LinearLayout filler = (LinearLayout) findViewById(R.id.filler);
+        setContentView(R.layout.activity_article);
 
-        // Add first the opening text.
-        View top = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(
-                R.layout.about_page_opening, null, false);
-        TextView opening = (TextView) top.findViewById(R.id.opening);
-        opening.setText((String) JsonParser.about_page_info.get("opening"));
-        filler.addView(top);
+        progress = new AboutProgressDialog(AboutActivity.this);
+        progress.show();
 
-        // Add all three snippets, two with the image on the left and one with the image on the
-        // right.
-        int idx = 0;
-        for (final Map<String, String> snippet : ((List<Map<String, String>>) JsonParser
-                .about_page_info.get("snippets"))) {
-            View snippet_view = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE))
-                    .inflate(R.layout.about_page_snippet, null);
-            final ImageView imageView = (ImageView) snippet_view.findViewById(R.id.snippet_image);
-            final TextView textView = (TextView) snippet_view.findViewById(R.id.snippet_text);
-            imageView.setBackground(ImageProcessor.about_snippet_images.get(idx));
-            // This creates text wrapping.
-            makeSpan(imageView, textView, snippet);
-            filler.addView(snippet_view);
-            idx++;
+        webView = (WebView) findViewById(R.id.web_view);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // This instantiates a new WebViewClient, so as not to require the use of a specific client
+        // such as Chrome.
+        webView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String
+                    failingUrl) {
+                Toast.makeText(getApplicationContext(), description, Toast.LENGTH_SHORT).show();
+            }
+            public void onPageFinished(WebView view, String url) {
+                progress.dismiss();
+            }
+        });
+
+        webView.getSettings().setBuiltInZoomControls(true); // Enable zoom.
+
+        webView.loadUrl(link);
+    }
+
+    // While in the WebView, the back button serves the WebView, unless pressed when at origin page,
+    // then it ends the activity and goes back to the main menu.
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(event.getAction() == KeyEvent.ACTION_DOWN){
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (webView.canGoBack()) {
+                    webView.goBack();
+                } else {
+                    // Go back to MainActivity.
+                    finish();
+                }
+                return true;
+            }
         }
+        return super.onKeyDown(keyCode, event);
+    }
 
-        // Add the goal and the people introduction.
-        View middle_view = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE))
-                .inflate(R.layout.about_page_middle, null, false);
-        TextView middle = (TextView) middle_view.findViewById(R.id.middle);
-        middle.setText(JsonParser.about_page_info.get("goal") + "\n\n" + JsonParser.about_page_info
-                .get("people_intro"));
-        filler.addView(middle_view);
-
-        // Add in all the people.
-        idx = 0;
-        for (final Map<String, String> person : ((List<Map<String, String>>) JsonParser
-                .about_page_info.get("people"))) {
-            View person_view = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE))
-                    .inflate(R.layout.about_page_person, null);
-            ImageView imageView = (ImageView) person_view.findViewById(R.id.person_image);
-            TextView caption = (TextView) person_view.findViewById(R.id.person_caption);
-            final TextView textView = (TextView) person_view.findViewById(R.id.person_text);
-            imageView.setBackground(ImageProcessor.about_people_images.get(idx));
-            caption.setText(person.get("caption"));
-            final LinearLayout linearLayout = (LinearLayout) person_view.findViewById(
-                    R.id.person_image_caption);
-            // This creates text wrapping.
-            makeSpan(linearLayout, textView, person);
-            filler.addView(person_view);
-            idx++;
-        }
-	}
+    @Override public void finish() {
+        ViewGroup view = (ViewGroup) getWindow().getDecorView();
+        view.removeAllViews(); // Avoids window leaking exception.
+        super.finish();
+    }
 
 /*
     Action bar functions:
@@ -167,66 +154,21 @@ public class AboutActivity extends Activity {
     }
 
 /*
-    Helper function:
+        This is the AboutProgressDialog dialog.
+        Its layout file is dialog_progress.xml.
 */
-    void makeSpan(final View image_v, final TextView text_v, final Map<String, String> block) {
-        final ViewTreeObserver vto = image_v.getViewTreeObserver();
-        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                image_v.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                int finalHeight = image_v.getMeasuredHeight();
-                int finalWidth = image_v.getMeasuredWidth();
-                String text = block.get("text");
+    public class AboutProgressDialog extends AlertDialog {
 
-                int allTextStart = 0;
-                int allTextEnd = text.length() - 1;
+        public AboutProgressDialog(Context context) {
+            super(context);
+        }
 
-                // Calculate the number of lines to indent.
-                int lines;
-                Rect bounds = new Rect();
-                text_v.getPaint().getTextBounds(text.substring(0,10), 0, 1, bounds);
-                float fontSpacing = text_v.getPaint().getFontSpacing();
-                lines = (int) (finalHeight/fontSpacing);
+        @Override
+        public void show() {
+            super.show();
+            setContentView(R.layout.dialog_progress);
+            ((TextView) findViewById(R.id.text)).setText("Loading About page...");
+        }
 
-                // Build the layout with LeadingMarginSpan2.
-                MyLeadingMarginSpan2 span = new MyLeadingMarginSpan2(lines, finalWidth +10 );
-                SpannableString SS = new SpannableString(text);
-                SS.setSpan(span, allTextStart, allTextEnd,
-                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                text_v.setText(SS);
-            }
-        });
     }
 }
-
-class MyLeadingMarginSpan2 implements LeadingMarginSpan.LeadingMarginSpan2 {
-    private int margin;
-    private int lines;
-
-    MyLeadingMarginSpan2(int lines, int margin) {
-        this.margin = margin;
-        this.lines = lines;
-    }
-
-    // Returns the value to which indentation must be added.
-    @Override
-    public int getLeadingMargin(boolean first) {
-        if (first) {
-            return margin;
-        }
-        else {
-            return 0;
-        }
-    }
-
-    @Override
-    public void drawLeadingMargin(Canvas c, Paint p, int x, int dir, int top, int baseline, int
-            bottom, CharSequence text, int start, int end, boolean first, Layout layout) {}
-
-    @Override
-    public int getLeadingMarginLineCount() {
-        return lines;
-    }
-}
-
